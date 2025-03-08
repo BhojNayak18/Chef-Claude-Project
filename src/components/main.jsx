cfeedda7a9bb20b9cf0d56943b003e7963633623
Bhoj Narayan Nayak <bhojnayak18@gmail.com>
2025-03-09 01:56:53 +0530

    Refactor ingredient handling in Main component with form action

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,35 +1,30 @@
 import { useState } from "react";
 
 export default function Main() {
-    const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-    const ingredientsListItems = ingredients.map((ingredient, index) => {
-        return <li key={index}>{ingredient}</li>
-    })
+  const ingredientsListItems = ingredients.map((ingredient, index) => {
+    return <li key={index}>{ingredient}</li>;
+  });
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const form = event.target
-        const formData = new FormData(form)
-        const newIngredient = formData.get("ingredient")
-        setIngredients(preIngredients => [...preIngredients, newIngredient])
-    }
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((preIngredients) => [...preIngredients, newIngredient]);
+  }
 
-    return (
-        <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
-                <input 
-                    type="text" 
-                    placeholder="e.g. oregano" 
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button type="submit">Add ingrediant</button>
-            </form>
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button type="submit">Add ingrediant</button>
+      </form>
 
-            <ul className="ingredient-list">
-                {ingredientsListItems}
-            </ul>
-        </main>
-    )
+      <ul className="ingredient-list">{ingredientsListItems}</ul>
+    </main>
+  );
 }

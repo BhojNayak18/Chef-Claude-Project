19624f41cfd345907c05370b28b59a41dc8986d2
Bhoj Narayan Nayak <bhojnayak18@gmail.com>
2025-03-11 02:01:09 +0530

    Conditional rendering for recipe prompt based on ingredient count in Main component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -24,19 +24,23 @@ export default function Main() {
         <button type="submit">Add ingrediant</button>
       </form>
 
-      <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredients-list" aria-live="polite">
-          {ingredientsListItems}
-        </ul>
-        <div className="get-recipe-container">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button>Get a recipe</button>
-        </div>
-      </section>
+      {ingredients.length ? (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          {ingredients.length > 3 ? (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          ) : null}
+        </section>
+      ) : null}
     </main>
   );
 }
